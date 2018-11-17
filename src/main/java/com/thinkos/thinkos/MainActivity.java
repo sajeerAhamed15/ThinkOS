3cb89c9b96b802a68207ae6101160e5184120675
Sajeer Ahamed <ahamedsajeer23@gmail.com>
2018-11-17 17:41:13 +0530

    init commit

---
+++ src/main/java/com/thinkos/thinkos/MainActivity.java
@@ -0,0 +1,28 @@
+package com.thinkos.thinkos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View view) {
+    }
+    public void click2(View view) {
+    }
+    public void click3(View view) {
+    }
+    public void click4(View view) {
+    }
+    public void click5(View view) {
+    }
+
+
+
+}
